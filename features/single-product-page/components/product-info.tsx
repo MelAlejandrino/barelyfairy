49bd7c9ab71ba12901d76b4d0de39af01e8479a4
MelAlejandrino <alejandrinomel02@gmail.com>
2026-03-17 08:26:58 +0800

    rename from sizes to variants

--- features/single-product-page/components/product-info.tsx
+++ features/single-product-page/components/product-info.tsx
@@ -40,7 +40,7 @@ export default function ProductInfo({product}: ProductInfoProps) {
             {product.hasVariants && product.variants?.length && (
                 <div className="flex flex-col gap-2">
                     <p className="text-[11px] font-medium uppercase tracking-widest text-rose-300">
-                        Sizes
+                        Variants
                     </p>
                     <div className="flex flex-wrap gap-2">
                         {product.variants.map((v) => (
